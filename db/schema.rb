468b6cf3411f7c3d1219c4fb5f62ddf0e28b0e59
dai <dev_daisuke0926@yahoo.co.jp>
2023-10-02 14:36:08 +0900

    generate devise's views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_025324) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_02_052158) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_025324) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "post_code"
+    t.string "addr"
+    t.string "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

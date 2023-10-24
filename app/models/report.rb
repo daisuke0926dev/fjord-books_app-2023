83c905a170194e5eb4047ecd84de7e795fc56e3d
dai <dev_daisuke0926@yahoo.co.jp>
2023-10-24 14:42:16 +0900

    rubocop, i18n

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentions, foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :mentions, foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :report
   has_many :mentioning_reports, through: :mentions, source: :mentioned_report
 
-  has_many :reverse_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy
+  has_many :reverse_mentions, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :report
   has_many :mentioned_reports, through: :reverse_mentions, source: :mentioning_report
 
   validates :title, presence: true

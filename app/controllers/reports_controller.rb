249a099b184dca56d14031c8fd209830fd9176a8
dai <dev_daisuke0926@yahoo.co.jp>
2023-11-17 18:41:24 +0900

    コントローラーの処理をモデルに移す

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,6 @@
 
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[edit update destroy]
-  after_action :update_mentions_from_content, only: %i[create destroy update]
 
   def index
     @reports = Report.includes(:user).order(id: :desc).page(params[:page])
@@ -23,6 +22,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
+      @report.update_mentions_from_content
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -31,6 +31,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      @report.update_mentions_from_content
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -39,6 +40,7 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
+    @report.update_mentions_from_content
 
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
@@ -51,24 +53,5 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
-  end
-
-  def update_mentions_from_content
-    mentioning_report_ids = extract_report_ids_from_content
-    current_mentioning_ids = @report.mentioning_reports.pluck(:id)
-
-    ids_to_remove = current_mentioning_ids - mentioning_report_ids
-    ids_to_add = mentioning_report_ids - current_mentioning_ids
-
-    Mention.where(mentioning_report_id: @report.id, mentioned_report_id: ids_to_remove).destroy_all
-    ids_to_add.each do |id|
-      Mention.create(mentioning_report_id: @report.id, mentioned_report_id: id)
-    end
-  end
-
-  def extract_report_ids_from_content
-    total_reports = Report.count
-    regex = %r{http://localhost:3000/reports/([1-#{total_reports}]+)}
-    @report.content.scan(regex).flatten.map(&:to_i)
   end
 end

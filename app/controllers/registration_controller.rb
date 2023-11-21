73875bf1ebe80a6df1a2cf9ec9bcc866112edf24
dai <90462400+daisuke0926dev@users.noreply.github.com>
2023-11-21 09:32:46 +0900

    不要なcontrollerを削除

--- app/controllers/registration_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RegistrationController < ApplicationController
-  def account_update
-    super
-    resource.avatar.attach(account_update_params[:avatar]) if account_update_params[:avatar].present?
-  end
-end
